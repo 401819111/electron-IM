447c087cd7d52e913a2ac960a1f98713cafd3242
lx401819111 <401819111@QQ.COM>
2021-06-16 23:26:03 +0800

    取消eslint,和prettier冲突太烦了

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: '/',
+      component: require('@/views/index').default
     },
     {
       path: '*',
---
+++ src/renderer/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped></style>
